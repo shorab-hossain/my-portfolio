bd158c0debedfd6c3ef6dc61b58798d40fefca27
= <=>
2022-09-09 10:58:41 +0600

    code push done

--- src/Components/Home/Home/Contract-form/Contract.js
+++ src/Components/Home/Home/Contract-form/Contract.js
@@ -1,78 +1,29 @@
 
-import React, { useState } from 'react';
+import emailjs from '@emailjs/browser';
+import React, { useRef, useState } from 'react';
 import Card from 'react-bootstrap/Card';
 import myImage from '../../../../image/shakil.jpg';
-
-import { createUserWithEmailAndPassword, getAuth, sendEmailVerification, signInWithEmailAndPassword } from 'firebase/auth';
-import initializeAuthentication from './../../../Firebase/Firebase.initialize';
 import './Contract.css';
 
-initializeAuthentication()
 const Contract = () => {
-
-    const [user, setUser] = useState({});
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [error,setError] = useState('')
-    const [isLogIn,setIsLogIn] = useState(false)
+    const form = useRef();
+    const [done,setDone] = useState(false);
     
- 
-    const auth = getAuth();
-
-      const handlleSubmit = e =>{
+    const sendEmail = (e) => {
         e.preventDefault();
-        if(password.length < 6){
-            setError('Password must be at least 6 characters long');
-            return;
-        }
-         if(!/(?=.*[0-9].*[0-9])/.test(password)){
-            setError('Password must be at least 2 number');
-            return;
-        }
-        if(isLogIn){
-            processLogIn(email,password);
-        }
-        else{
-            createNewUser(email,password)
-        }
-        
-      }
-      const toggleChecked = e =>{
-        setIsLogIn(e.target.checked)
-      }
-      const handleEmail = e =>{
-        setEmail(e.target.value)
-      }
-      const handlePassword = e =>{
-        setPassword(e.target.value)
-      }
-      const processLogIn =(email,password) =>{
-        signInWithEmailAndPassword (auth,email,password)
-        .then(result =>{
-                const user = result.user;
-                console.log(user)
-               setError('')
-                
-        })
-        .catch((error) =>{
-            const errorMessage = error.message;
-            console.log(errorMessage)
-        })
-      }
-      const createNewUser = (email,password) =>{
-        createUserWithEmailAndPassword (email,password)
-        .then(result =>{
-            const user = result.user;
-            console.log(user)
-            setError('')
-             handleEmailVerify()
-        })
-      }
-      const handleEmailVerify =() =>{
-        sendEmailVerification(auth.currentUser)
-        .then(() => {
-     });
-      }
+    
+        emailjs.sendForm('service_vklamzn', 'template_tmt21ky', form.current, 'rWbHLgpjfv4KHXkZ3')
+          .then((result) => {
+              console.log(result.text);
+              setDone(true)
+          }, (error) => {
+              console.log(error.text);
+              setDone('')
+          });
+          e.target.reset();
+          
+      };
+     
     return (
         <div className="bg-dark">
                 <div className="container py-5">
@@ -85,52 +36,38 @@ const Contract = () => {
                                         <Card.Text>
                                         <p>I am a front-end Developer.I worked in this field in many year. I have learned a lot of language. There are like HTML5,CSS,BOOTSTRAP, JAVASCRIPT, REACT and etc.</p>
                                         </Card.Text>
-                                        {
-                                            user.email && <div>
-                                                <h4>User name :{user.name}</h4>
-                                                <p>User email : {user.email}</p>
-                                                <img src={user.img} alt=""/>
-                                            </div>
-                                    }
-                                        {
-                                            user.name && <div>
-                                                <h4>User name :{user.name}</h4>
-                                                <p>User email : {user.email}</p>
-                                                <img src={user.img} alt=""/>
-                                            </div>
-                                    }
                                     </Card.Body>
                                  </Card>
                              </div>
                              <div className='col-md-6 col-sm-12 col-lg-6'>
                                 
-                             <form className='text-light' >
-                             <h4 className="mb-3 text-light">{isLogIn ? 'LogIn':'Registered'}</h4>
+                             <form ref={form} onSubmit={sendEmail} className='text-light' >
+                             <h4 className="mb-3 text-light">Contract Me</h4>
                                     <div className="d-flex ">
                                         <div className='me-4'>
-                                                <label className='label-design' htmlFor="fname">Email:</label><br/>  
-                                            <input onBlur={handleEmail} className='form-design' type='email' required />
+                                                <label className='label-design' htmlFor="fname">Name</label><br/>  
+                                            <input className='form-design' type='text' name="user_name" required />
+                                           
+                                        </div>
+                                        <div className='me-4'>
+                                                <label className='label-design' htmlFor="email">Email</label><br/>  
+                                            <input className='form-design' type='email' name="user_email" required />
                                            
                                         </div>
                                         <div>
-                                            {/* <label className='label-design' htmlFor="fname">Password</label><br/>  
-                                        <input onBlur={handlePassword} className='form-design' type='password' required /> */}
                                         </div>
                                     </div>
                                     <div className="mt-0 mb-4">
-                                        <textarea  className="form-message form-design" id="exampleFormControlTextarea1 " rows="3" placeholder='Write Your message'></textarea>
+                                        <textarea  className="form-message form-design" id="exampleFormControlTextarea1 " rows="3" placeholder='Write Your message' name="message"></textarea>
                                         </div>
-                                        <div className="mb-3 text-danger">
-                                            {error}
-                                        </div>
-                                        <div className="form-check">
-                                        <input onChange={toggleChecked} className="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
-                                        <label className="form-check-label" htmlFor="flexCheckDefault">
-                                            Already Registered?
-                                        </label>
-                                    </div>
-                                        <button onClick={handlleSubmit}  type="submit" className="btn btn-primary py-2 px-4 fs-5 fw-bold mt-4">{isLogIn ? 'Log in' :'Register'}</button>
+                                        <button type="submit" className="btn btn-primary py-2 px-4 fs-5 fw-bold mt-4" value="send"> Submit</button>
                                     </form>
+                                    <div>
+                                        {
+                                            done && <p className='text-success text-center fs-5'>Your form submit done!!</p>
+                                           
+                                        }
+                                    </div>
                                 </div>
                         </div>        
                     </div>
